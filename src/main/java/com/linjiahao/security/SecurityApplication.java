ba466a52c692298e60666da331ea6b66cd0f2605
HapiFive <1143910315@qq.com>
2019-05-31 21:06:19 +0800

    配置CSRF过滤器

--- src/main/java/com/linjiahao/security/SecurityApplication.java
+++ src/main/java/com/linjiahao/security/SecurityApplication.java
@@ -2,6 +2,10 @@ package com.linjiahao.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
 @SpringBootApplication
 public class SecurityApplication {
@@ -10,4 +14,17 @@ public class SecurityApplication {
         SpringApplication.run(SecurityApplication.class, args);
     }
 
+    /**
+     * 配置CSRF过滤器
+     *
+     * @return {@link org.springframework.boot.web.servlet.FilterRegistrationBean}
+     */
+    @Bean
+    public FilterRegistrationBean<CsrfFilter> csrfFilter() {
+        FilterRegistrationBean<CsrfFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new CsrfFilter(new HttpSessionCsrfTokenRepository()));
+        registration.addUrlPatterns("/*");
+        registration.setName("csrfFilter");
+        return registration;
+    }
 }
